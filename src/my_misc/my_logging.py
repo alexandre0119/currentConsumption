0d42d08959fe2364ef8b75dd77f7f75eda9f614d
AlexWang <alexandre0119@gmail.com>
2017-03-05 18:38:07 -0800

    Fix logging

--- src/my_misc/my_logging.py
+++ src/my_misc/my_logging.py
@@ -3,34 +3,37 @@
 # Author: Alex Wang # This file is cited online
 
 import sys
-import os
 import logging
 
-# create logger, write to a file
-def create_logger(logger_name='default', log_file='log_this.txt'):
 
+def create_logger(logger_name=__name__, log_file='log_this.txt'):
 	logger = logging.getLogger(logger_name)
-	logger.setLevel(logging.DEBUG)
 
-	# create file handler
-	fh = logging.FileHandler(log_file, mode='a')
-	fh.setLevel(logging.DEBUG)
+	if logger.handlers:
+		return logger
+	else:
+		logger = logging.getLogger(logger_name)
+		logger.setLevel(logging.DEBUG)
 
-	# create console handler
-	sh = logging.StreamHandler(stream=sys.stdout)
-	sh.setLevel(logging.DEBUG)
+		# create file handler
+		fh = logging.FileHandler(log_file, mode='a')
+		fh.setLevel(logging.DEBUG)
 
-	# create formatter and add it to the handlers
-	formatter = logging.Formatter('[%(asctime)s] %(levelname)8s --- %(message)s ' +
-	                               '(%(filename)s:%(lineno)s)', datefmt='%Y-%m-%d %H:%M:%S')
+		# create console handler
+		ch = logging.StreamHandler(stream=sys.stdout)
+		ch.setLevel(logging.DEBUG)
 
-	fh.setFormatter(formatter)
-	sh.setFormatter(formatter)
+		# create formatter and add it to the handlers
+		# formatter = logging.Formatter('[%(asctime)s] %(levelname)8s --- %(message)s' +
+		#                                '(%(filename)s:%(lineno)s)', datefmt='%Y-%m-%d %H:%M:%S')
 
-	# add the handlers to the logger
-	logger.addHandler(sh)
-	logger.addHandler(fh)
+		formatter = logging.Formatter('[%(asctime)s] %(levelname)8s --- %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-	return logger
+		fh.setFormatter(formatter)
+		ch.setFormatter(formatter)
 
+		# add the handlers to the logger
+		logger.addHandler(ch)
+		logger.addHandler(fh)
 
+		return logger
